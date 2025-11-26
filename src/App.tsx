588bd214b1af2ee6b30882fc5e19da42a00c34c6
aerials <nosvengeance@gmail.com>
2025-11-26 13:34:29 +0700

    feat: setup routing & lenis

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import { useGSAP } from "@gsap/react";
+import Lenis from "lenis";
+
 import AppRouter from "./router";
 
+gsap.registerPlugin(SplitText, ScrollTrigger);
+
 function App() {
+  const lenis = new Lenis();
+
+  lenis.on("scroll", ScrollTrigger.update);
+
+  useGSAP(() => {
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 500); // Convert time from seconds to milliseconds
+    });
+
+    gsap.ticker.lagSmoothing(0);
+  }, []);
+
   return <AppRouter />;
 }
 
